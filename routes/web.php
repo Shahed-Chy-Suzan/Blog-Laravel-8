ecc20ff6ea4df89854cbfcb6dfa7e64022148fe6
Shahed Chy Suzan <shahedchysuzan@gmail.com>
2021-11-04 20:59:39 +0600

    Integrating Template with Laravel

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\BlogController;
+use App\Http\Controllers\ContactController;
+use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +16,30 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// www.alphayo.com
+// www.alphayo.com/
+
+// Using closure
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
+// Using controller
+
+// To welcome page
+Route::get('/', [WelcomeController::class, 'index'])->name('welcome.index');
+
+// To blog page
+Route::get('/blog', [BlogController::class, 'index'])->name('blog.index');
+
+// To single blog post
+Route::get('/blog/single-blog-post', [BlogController::class, 'show']);
+
+// To about page
+Route::get('/about', function(){
+    return view('about');
 });
+
+// To contact page
+Route::get('/contact', [ContactController::class, 'index']);
