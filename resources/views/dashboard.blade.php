9e44af1d16d3cc358c76d7be38b863a55311c8b4
Shahed Chy Suzan <shahedchysuzan@gmail.com>
2021-11-06 02:04:46 +0600

    Post Create Page

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -14,6 +14,15 @@
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6 bg-white border-b border-gray-200">
                         You're logged in! {{ Auth::user()->name }}
+
+                        <div class="dashboard">
+                            <ul>
+                                <li><a href="{{route('blog.create')}}">Create Post</a></li>
+                                <li><a href="">Create Category</a></li>
+                                <li><a href="">Categories List</a></li>
+                            </ul>
+                        </div>
+                        
                     </div>
                 </div>
             </div>
