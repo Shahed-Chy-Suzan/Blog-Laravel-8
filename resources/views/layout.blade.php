48a74f5ddda20d25ac0b4f2c5bad34702b6a0306
Shahed Chy Suzan <shahedchysuzan@gmail.com>
2021-11-11 01:41:29 +0600

    Customizing Pagination View with Bootstrap

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -20,7 +20,11 @@
     @yield('head')
 
     {{-- Tailwind CDN --}}
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.2.17/tailwind.min.css">
+    {{-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.2.17/tailwind.min.css"> --}}
+
+    {{-- Bootstrap CDN --}}
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+
 </head>
 
 <body>
